607283b6bef3790ad224d80ff5a2c9cdaa645219
Prateek Rawat <prateekj297@gmail.com>
2022-02-03 18:19:26 +0530

    Add logging to functions

--- sales.py
+++ sales.py
@@ -11,6 +11,7 @@ class Sales(Data):
         :param df:
         :return:
         """
+        self.mylogger.info("Calculate total revenue by Regions")
         newdf = df.groupBy('Region').agg(sum('Total Revenue').alias("Total Revenues"))
         return newdf
 
@@ -21,8 +22,9 @@ class Sales(Data):
         :param item: Item Type
         :return:
         """
+        self.mylogger.info("calculate Top 5 Countries by Item Type and Units Sold")
         newdf = df.filter(df["Item Type"] == item)
-        newdf = newdf.orderBy(df['Units Sold']).limit(5)
+        newdf = newdf.orderBy(newdf['Units Sold']).limit(5)
         return newdf
 
     def totalProfit(self, df):
@@ -33,6 +35,7 @@ class Sales(Data):
         """
         # TODO: Remove hardcoded Region and date range
 
+        self.mylogger.info("Calculating total profit by region")
         newdf = df.filter(df['Region'] == "Asia").filter(df['Order Date'].between('2011-01-01', '2015-12-31')).orderBy(
             df['Order Date'])
         return newdf
